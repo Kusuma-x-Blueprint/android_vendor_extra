00bea65fd132c85e25abbc87c754ac5e09fce04d
someone5678 <someone5678@users.noreply.github.com>
2023-12-01 22:47:41 +0900

    extra: Guard Face Unlock support
    * There're some devices with native face unlock support which ParanoidSense is not required
    * So let's not enforce build for weak modality face unlock
    
    Change-Id: I0f8d34bd29fa8c47384af1e178bd827a23027841

--- product.mk
+++ product.mk
@@ -21,7 +21,8 @@ PRODUCT_PACKAGES += \
     Cromite
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
 
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
@@ -32,6 +33,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 endif
 
 # Fonts
