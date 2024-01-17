c6ae0dc0a6baddf1ca01e47891931d9b91bb0228
Praditia Nur <pnplusplus@gmail.com>
2024-01-17 15:16:32 +0000

    extra: Guard different sign method * If there's no .android-certs directory, then build process will using default test-keys
    Change-Id: I8805ef59a7a66d4013cea1213170a0535b8619e1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,9 @@
 export TARGET_UNOFFICIAL_BUILD_ID=KUSUMA
 
 # Sign with own key
+export OWN_KEYS_DIR=
+if [ -d "$HOME/.android-certs" ]; then
 export OWN_KEYS_DIR=~/.android-certs
+else
+echo -e "There's no android-certs directory.\nPlease make it using tutorial from:\nhttps://wiki.lineageos.org/signing_builds#generating-the-keys\nWill be using default test-keys."
+fi
